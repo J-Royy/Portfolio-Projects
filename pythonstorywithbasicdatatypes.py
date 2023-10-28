3b7e0d8f18741a3495ec866416e2bc9a6572dc68
Jamie M <146683101+J-Royy@users.noreply.github.com>
2023-10-27 23:36:25 -0500

    short python story with basic data types

---
+++ pythonstorywithbasicdatatypes.py
@@ -0,0 +1,20 @@
+character_job = 'Sheriff'
+character_age = 29
+mainchar = 'Rick'
+quick = 'A SHORT STORY.'
+
+#--3 basic types of data--
+#strings = character(s) in quotations ex. print('Hello World')
+#numbers dont need to be in quotes
+#boolean values are true/false values (ex. is_male = True)
+#/n for new line +
+#functions ex. ".lower()" etc
+#len for length of characters ex. print(len(quick))
+#print(quick.index('H')) = 3 | indexing function
+#print(quick.replace('A', 'The')) | replacing function
+
+print('This is ' + quick.lower())
+print('There was a ' + character_job + ' named Rick Grimes,')
+print('he was 29 years old at the start of initial outbreak.')
+print(mainchar + ' loved his son Carl,')
+print('but he wanted a daughter as well. \nEND OF STORY')
