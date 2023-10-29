1b107de99fa02a50cd298ed2270e32aac33a8da0
Jamie M <146683101+J-Royy@users.noreply.github.com>
2023-10-29 02:18:27 -0500

    basic addition calculator
    made this after practicing input function xD

---
+++ basic addition calculator.py
@@ -0,0 +1,11 @@
+#use int function for whole numbers
+#use float function for decimal
+
+
+
+print('Welcome to my addition calculator. Let\'s do some addition! ')
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+result = float(num1) + float(num2)
+
+print(result)
