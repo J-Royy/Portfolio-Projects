320420df0d32e71242edd516338f51a04161990f
Jamie M <146683101+J-Royy@users.noreply.github.com>
2023-11-03 22:34:49 -0500

    more function practice
    functions with passing in arguments has me slightly confused. getting better! will also work on return function next

---
+++ more function practice.py
@@ -0,0 +1,6 @@
+def say_hi(name, age):
+    print('Yo ' + name + ' ,you are ' + age + ' years old! You should try to learn Python!')
+
+custom = input('Enter name for message!: ')
+age = input('Enter an age!: ')
+say_hi(custom,age)
